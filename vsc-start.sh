a76aea13681f24433a3fc38d3e9ece072b2a13d3
g6re <111838552+g6re@users.noreply.github.com>
2024-12-02 21:55:20 +0100

    .

--- vsc-start.sh
+++ vsc-start.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-# Nombre de la sesión
 SESSION_NAME="my_project"
 
-# Directorios del frontend y backend
 FRONTEND_DIR="$HOME/store/store-frontend"
 BACKEND_DIR="$HOME/store/store-backend"
 
-# Comprobar si ya existe la sesión
 tmux has-session -t $SESSION_NAME 2>/dev/null
 
 if [ $? != 0 ]; then
-    # Crear una nueva sesión y ventana para el frontend
     tmux new-session -d -s $SESSION_NAME -n frontend
     tmux send-keys -t $SESSION_NAME:0 "cd $FRONTEND_DIR && npm start" C-m
 
-    # Crear una nueva ventana para el backend
     tmux new-window -t $SESSION_NAME -n backend
     tmux send-keys -t $SESSION_NAME:1 "cd $BACKEND_DIR && npm start" C-m
 fi
 
-# Adjuntar a la sesión
 tmux attach-session -t $SESSION_NAME
 
